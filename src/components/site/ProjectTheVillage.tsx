94b292e5bff021074ce4d4ff10ead57cb554ecf6
Wilmond <wilmond@Wilmonds-MacBook-Air.local>
2026-06-30 07:58:38 +0800

    Responsive marquee speed: 12s mobile, 10s desktop

--- src/components/site/ProjectTheVillage.tsx
+++ src/components/site/ProjectTheVillage.tsx
@@ -2,6 +2,7 @@ import { SectionLabel } from "./SectionLabel";
 import { PageNumber } from "./PageNumber";
 import { Reveal } from "./Reveal";
 import { Marquee } from "./Marquee";
+import { useIsMobile } from "@/hooks/use-mobile";
 import logo from "@/assets/projects/the-village/logo.png";
 import openHours from "@/assets/projects/the-village/open-hours.jpg";
 import menu from "@/assets/projects/the-village/menu.jpg";
@@ -9,6 +10,8 @@ import eventPoster from "@/assets/projects/the-village/event-poster.jpg";
 import testimonial from "@/assets/projects/the-village/testimonial.png";
 
 export function ProjectTheVillage() {
+  const isMobile = useIsMobile();
+
   return (
     <section id="the-village" className="bg-cream text-chocolate py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-5 sm:px-8">
@@ -39,7 +42,10 @@ export function ProjectTheVillage() {
         </div>
 
         <Reveal className="mt-12">
-          <Marquee images={[logo, menu, openHours, eventPoster, testimonial]} speed={10} />
+          <Marquee
+            images={[logo, menu, openHours, eventPoster, testimonial]}
+            speed={isMobile ? 12 : 10}
+          />
         </Reveal>
       </div>
     </section>
